ce42218b018a83b5797e9339b825b268952a4976
Pawan Vedak <vedaakp@gmail.com>
2022-09-25 12:43:18 +0530

    Add files via upload

---
+++ sound.js
@@ -0,0 +1,24 @@
+var audioContext = new (window.AudioContext || window.webkitAudioContext)();
+var stopTime = audioContext.currentTime;
+var oscillatorNode = audioContext.createOscillator();
+oscillatorNode.type = "sine";
+oscillatorNode.connect(audioContext.destination);
+oscillatorNode.start (); //Play beep!!!!!
+
+function beep(frequency,durationSec,ramp=false){
+    if (ramp) {
+        oscillatorNode.frequency.linearRampToValueAtTime (frequency, stopTime); // value in hertz
+    } else {
+        oscillatorNode.frequency.setValueAtTime (frequency, stopTime);  // value in hertz
+    }
+    stopTime += durationSec;
+    oscillatorNode.stop (stopTime);
+}
+
+
+beep (250, 0.5);
+beep (1000, 0.2);
+beep (550, 0.5);
+beep (50, 2, true);
+beep (5000, 2, true);
+beep (50, 0, true);
