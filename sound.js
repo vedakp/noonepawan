0ab791c7db1a9224cc093f0f1fcf488447449caa
Pawan Vedak <vedaakp@gmail.com>
2022-09-25 13:09:55 +0530

    Update sound.js

--- sound.js
+++ sound.js
@@ -1,31 +1,41 @@
-var oscillatorNodeStarted = false;
+var audioContext = null;
+var oscillatorNode = null;
+var stopTime = 0;
 
-function beep(frequency,durationSec,ramp=false){
-    var audioContext = new (window.AudioContext || window.webkitAudioContext)();
-    var stopTime = audioContext.currentTime;
-    var oscillatorNode = audioContext.createOscillator();
-    oscillatorNode.type = "sine";
-    oscillatorNode.connect(audioContext.destination);
-    if(!oscillatorNodeStarted){
-        oscillatorNode.start (); //Play beep!!!!!
-        oscillatorNodeStarted = true;
+function beep (frequency, durationSec, ramp=false)
+{
+    if (oscillatorNode == null) {
+        audioContext = new (window.AudioContext || window.webkitAudioContext) ();
+        stopTime = audioContext.currentTime;
+
+        oscillatorNode = audioContext.createOscillator();
+        oscillatorNode.type = "sine";
+        oscillatorNode.connect (audioContext.destination);
+        if (ramp) {
+            oscillatorNode.frequency.setValueAtTime (frequency, stopTime);
+        }
+        oscillatorNode.start ();
+        oscillatorNode.onended = function() {
+            oscillatorNode = null;
+            audioContext = null;
+        }
     }
-    
+
     if (ramp) {
         oscillatorNode.frequency.linearRampToValueAtTime (frequency, stopTime); // value in hertz
     } else {
         oscillatorNode.frequency.setValueAtTime (frequency, stopTime);  // value in hertz
     }
+
     stopTime += durationSec;
     oscillatorNode.stop (stopTime);
 }
 
 function playSound(){
-    console.log("playsound");
-    beep (250, 0.5);
-    beep (1000, 0.2);
-    beep (550, 0.5);
-    beep (50, 2, true);
-    beep (5000, 2, true);
-    beep (50, 0, true);
+  beep (250, 0.5);
+  beep (1000, 0.2);
+  beep (550, 0.5);
+  beep (50, 2, true);
+  beep (5000, 2, true);
+  beep (50, 0, true);
 }
